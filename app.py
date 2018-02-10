69f916db8a198586e6883cb8d534ccaa2c9cb9ca
Ariana Moreno <aremoren@ucsc.edu>
2018-02-10 08:01:22 +0000

    app.py edited online with Bitbucket

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, 
+from flask import request, make_response, jsonify
 
 
 app = Flask(__name__)
@@ -8,18 +9,18 @@ HashTable = {}
 
 def gov(key_name):
 	if (request.method == 'GET'):
-		return getValue(key_name)
+		return getVal(key_name)
 
 	else if (request.method == 'PUT') or (request.method == 'POST'):
 		v = request.form['val']
-		return putValue(key_name,v)
+		return putVal(key_name,v)
 
 	else if (request.method == 'DELETE'):
-		return deleteValue(key_name)
+		return deleteVal(key_name)
 	else:
 		return "Invalid request."
 
-def getValue(key):
+def getVal(key):
 	if (key in HashTable):
 		value = HashTable[key]
 		j = jsonify(msg='success',value=value)
@@ -29,17 +30,17 @@ def getValue(key):
 		return make_response(j,404,{'Content-Type':'application/json'})
 
 
-def putValue(key,value):
+def putVal(key,value):
 	if (key in HashTable):
 		HashTable[key]=value
 		j = jsonify(replaced=1, msg='success')
-		return make_response(j,200, {'Content-Type' : 'application/json'})
+		return make_response(j,200, {'Content-Type': 'application/json'})
 	else:
 		HashTable[key]=value
 		j = jsonify(replaced=0, msg='success')
-		return make_response(j,201,{'Content-Type' : 'application/json'})
+		return make_response(j,201,{'Content-Type': 'application/json'})
 
-def deleteValue(key):
+def deleteVal(key):
 	if (key in HashTable):
 		HashTable.pop(key,None)
 		j = jsonify(msg='success')
@@ -47,7 +48,6 @@ def deleteValue(key):
 	else:
 		j = jsonify(msg='error',error='key does not exist')
 		return make_response(j,404,{'Content-Type':'application/json'})
-
 if __name__ == '__main__':
 	app.debug = True
 	app.run(host='0.0.0.0', port=8080)
